5fe29fcf86ecaefe4a85fbd3a4875b6b0622d0f8
Zarinha.Hub <sadfrog215@gmail.com>
2026-02-18 20:43:00 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,17 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, ".", "");
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: "/zb-barbearia/", // nome do repo no GitHub
+    base: "/zb-barbearia/",
+
+    plugins: [react()],
 
     server: {
       port: 3000,
       host: "0.0.0.0",
     },
-
-    plugins: [react()],
 
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
@@ -22,7 +22,7 @@ export default defineConfig(({ mode }) => {
 
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "."),
+        "@": path.resolve(process.cwd(), "."),
       },
     },
   };
